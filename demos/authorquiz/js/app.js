13e55c3b8f210948605f5dc2ccfc1342ddf138d6
karthikpamidimarri <karthikpamidimari@gmail.com>
2018-02-25 12:28:45 +0530

    Initialize component with getInitialState function.

--- demos/authorquiz/js/app.js
+++ demos/authorquiz/js/app.js
@@ -5,6 +5,7 @@
     var Quiz = React.createClass({
         render: function () {
             return <div>
+                <Count/>
             {this.props.books.map(function (b) {
                 return <Book title={b} />
             })}
@@ -14,10 +15,29 @@
 
     var Book = React.createClass({
         render : function () {
-            return <div><h4>{this.props.title}</h4></div>;
+            return <div><h4>{this.props.title}</h4><showState/></div>;
         }
     });
 
+    var showState = React.createClass({
+        getInitialState: function () {
+            return {answer: 42};
+        },
+        render: function () {
+            return <div>my state is {this.state.answer}</div>;
+        }
+    });
+
+    var Count = React.createClass({
+       getInitialState: function () {
+           return {counter: 1}
+       },
+       render: function () {
+           return <div><h1>Counter</h1>
+                <p>{this.state.counter}</p>
+           </div>
+       }
+    });
     React.renderComponent(<Quiz books= {['The Lord of The Rings', 'The Iliad']}/>,
         document.getElementById('app'));
 
